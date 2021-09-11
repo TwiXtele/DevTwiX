cf53a0894a9a9e5f4f2e8ec7e3d8701def99cfbc
Soerce TeAm TwiX <85749573+TwiXtele@users.noreply.github.com>
2021-09-11 18:52:30 +0300

    Add files via upload

--- DevTwix.lua
+++ DevTwix.lua
@@ -11253,7 +11253,7 @@ if SecondSudo(msg) then
 if text == "تحديث السورس" and SourceCh(msg) or text == "تحديث سورس" and SourceCh(msg) or text == "← تحديث السورس ᥀" and SourceCh(msg) then 
 Dev_HmD(msg.chat_id_, msg.id_, 1, '✫∫ جاري تحديث سورس بروك', 1, 'md') 
 os.execute('rm -rf DevTwix.lua') 
-os.execute('wget https://raw.githubusercontent.com/THE-DevTwix/DevTwix/main/DevTwix.lua') 
+os.execute('wget https://raw.githubusercontent.com/TwiXtele/DevTwix/main/DevTwix.lua') 
 dofile('DevTwix.lua') 
 io.popen("rm -rf ../.telegram-cli/*")
 print("\27[31;47m\n          ( تم تحديث السورس )          \n\27[0;34;49m\n") 
