c045ac401672c8956d2bd28498ad99d601db7ff0
Soerce TeAm TwiX <85749573+TwiXtele@users.noreply.github.com>
2021-09-28 15:43:52 +0300

    Add files via upload

--- DevTwix.lua
+++ DevTwix.lua
@@ -2225,6 +2225,7 @@ local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end,nil)
 end end
+end
 ---------------------------------------------------------------------------------------
 if not SecondSudo(msg) and not DevHmD:sismember(DevTwix..'HmD:Ban:Pv',msg.sender_user_id_) and not DevHmD:get(DevTwix..'HmD:Texting:Pv') then
 tdcli_function({ID="GetUser",user_id_=DevId},function(arg,chat) 
